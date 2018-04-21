3da062045f297f2124807e7a3e45f8e93128620a
Deformator <dammandrii@gmail.com>
2018-04-20 21:19:39 -0400

    Add validation to feedback form

--- src/pages/feedback/feedback.ts
+++ src/pages/feedback/feedback.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { FirebaseProvider } from '../../providers/firebase/firebase';
 import { DatePipe } from '@angular/common';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 
 @Component({
@@ -10,35 +11,37 @@ import { DatePipe } from '@angular/common';
 })
 export class FeedbackPage {
 
+  private feedback : FormGroup;
+
   allFeedbacks = [];
 
-  feedback = {
-    name: '',
-    body: '',
-    time: ''
-  }
-
-  constructor(private datePipe: DatePipe, private fireProvider: FirebaseProvider, public navCtrl: NavController) {
+  constructor(private formBuilder: FormBuilder, private datePipe: DatePipe, private fireProvider: FirebaseProvider, public navCtrl: NavController) {
     this.initializeItems()
+    
   }
 
   initializeItems(){
     this.fireProvider.getFeedbacks().then((response)=>{
       response.valueChanges().subscribe((feedbacks)=>{
         feedbacks.reverse()
-        console.log()
         this.allFeedbacks = feedbacks
       })
     })
+
+    this.feedback = this.formBuilder.group({
+      name: ['', Validators.required],
+      body: ['', Validators.required],
+      time: ''
+    });
   }
 
   sendFeedback(){
     let date = Date.now()
     let myFormattedDate = this.datePipe.transform(date, 'short')
-    this.feedback.time = myFormattedDate
-    this.fireProvider.addFeedback(this.feedback).then(()=>{
-      this.feedback.name = '';
-      this.feedback.body = '';
+    this.feedback.value.time = myFormattedDate
+    this.fireProvider.addFeedback(this.feedback.value).then(()=>{
+      this.feedback.reset()
+    
     })
   }
 
